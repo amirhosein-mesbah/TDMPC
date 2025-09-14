4a21c354643c9dfad430ae45bb0f2201c00ff4bc
amir <mesbahamirhossein@gmail.com>
2025-09-15 02:01:09 +0330

    imported libraries

--- tdmpc.py
+++ tdmpc.py
@@ -1,4 +1,17 @@
+import os
+import random
+import time
+from dataclasses import dataclass
 
+
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tyro
+from torch.distributions.categorical import Categorical
+from torch.utils.tensorboard import SummaryWriter
 
 if __name__=='__main__':
     print('test')
