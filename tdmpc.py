2980ef54b3b7f8ed0c043c6a0cde15dc7d1de92d
amir <mesbahamirhossein@gmail.com>
2025-09-27 20:29:34 +0330

    imported libraries

--- tdmpc.py
+++ tdmpc.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 
 
 import gymnasium as gym
+import metaworld
 import numpy as np
 import torch
 import torch.nn as nn
@@ -13,5 +14,17 @@ import tyro
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
 
+
+@dataclass
+class Args:
+    pass
+
+
+
 if __name__=='__main__':
     print('test')
+    env = gym.make('Meta-World/MT1', env_name='reach-v3')
+
+    obs = env.reset()
+    a = env.action_space.sample()
+    next_obs, reward, terminate, truncate, info = env.step(a)
